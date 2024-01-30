3736b0390a8cd8402577c72a127cedabb109fb86
Tonie Victor <victornwanochi@gmail.com>
2024-01-30 08:18:25 -0800

    testing

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,16 +9,17 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	size_t left_height, right_height;
 
-	if (tree == NULL)
-		return (-1);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+	if (tree != NULL)
+	{
+		left_height = binary_tree_height(tree->left);
+		right_height = binary_tree_height(tree->right);
+		
+		if (left_height > right_height)
+			return (left_height + 1);
+		else
+			return (right_height + 1);
+	}
+	return (0);
 }
