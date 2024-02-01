b5148db946044ac3b34205ec1411d95dc584162f
Tonie Victor <victornwanochi@gmail.com>
2024-02-01 00:13:08 -0800

    fix: Is Perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,28 +1,52 @@
 #include "binary_trees.h"
 #include <stdio.h>
 
+int power(int base, int exponent);
+
 /**
- * binary_tree_balance - a function that measures the
- * balance factor of a binary tree
+ * binary_tree_is_perfect - a function that checks if
+ * a binary tree is perfect
  * @tree: a pointer to the root node of the tree
  *
- * Return: the balance factor
+ * Return: 1 if the tree is perfect or 0 if not.
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int right, left;
+	int height, nodes, perf_nodes;
 
 	if (tree == NULL)
 		return (0);
 
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
+	height = binary_tree_height(tree);
+	nodes = binary_tree_size(tree);
 
-	if (left != right)
+	perf_nodes = power(2, height + 1) - 1;
+
+	if (nodes == perf_nodes)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * binary_tree_size - a function that measures the size of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the size
+ *
+ * Return: the size of the tree or zero on error
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_size, right_size;
+
+	if (tree == NULL)
 		return (0);
 
-	return (1);
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
+
+	return ((left_size + right_size) + 1);
 }
 
 /**
@@ -39,7 +63,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	height = 1;
+	height = 0;
+
+	if (tree->left != NULL || tree->right != NULL)
+		height += 1;
 
 	l_height = binary_tree_height(tree->left);
 	r_height = binary_tree_height(tree->right);
@@ -52,4 +79,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	height += r_height;
 	return (height);
+}
+
+/**
+ * power - a function to find the value of an exponential calculation.
+ * @base: the base value .
+ * @exponent: a non-negative value.
+ *
+ * Return: the result of the base raised to the power of the exponent.
+ */
+
+int power(int base, int exponent)
+{
+	if (exponent == 0)
+		return (1);
+
+	return (base * power(base, exponent - 1));
 }
