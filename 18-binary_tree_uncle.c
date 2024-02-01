27909037b5c2a9d5bf9d02db62cc4f11bc8b17a7
Tonie Victor <victornwanochi@gmail.com>
2024-02-01 00:57:21 -0800

    fix: Uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	return binary_tree_sibling(node->parent);
+	return (binary_tree_sibling(node->parent));
 }
 
 /**
